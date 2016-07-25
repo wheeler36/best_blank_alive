ceb35b93aaa0994475ec8fed19087d0de57e6524
wheeler36 <wheeler.hord@gmail.com>
2016-07-25 14:33:16 -0800

    Add extra fields to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,19 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :posts
+  has_many :posts, dependent: :destroy
+
+  validates :username,
+  :presence => true,
+  :uniqueness => {
+    :case_sensitive => false
+  }
+
+  validate :validate_username
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
 end
