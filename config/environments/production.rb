10c307733db1b32b50d5b1c315950015f121965c
wheeler36 <wheeler.hord@gmail.com>
2016-07-28 13:34:45 -0800

    Add secrets with ENV variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,8 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   Rails.application.configure do
-    ...
     config.secret_key_base = ENV["SECRET_KEY_BASE"]
-    ...
   end
+
 end
